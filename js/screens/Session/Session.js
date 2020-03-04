721565af026c2c3fe12dbb49803317a22ee2f356
Brooke <brooke.xiang@gmail.com>
2020-03-03 22:44:35 -0800

    add session screen directory and files

---
+++ js/screens/Session/Session.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Session = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Session Screen</Text>
+    </View>
+  );
+};
+
+export default Session;
---
+++ js/screens/Session/SessionContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import {Query} from '@apollo/react-components';
+import Session from './Session';
+
+const GET_SESSION = gql``;
+
+export default Session;
