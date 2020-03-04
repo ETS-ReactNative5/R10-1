fe56a58644a1b45c650684bc5628a6da666032d1
Brooke <brooke.xiang@gmail.com>
2020-03-03 22:43:35 -0800

    add Faves directory

---
+++ js/screens/Faves/Faves.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Faves extends React.Component {
+  render() {
+    return <Text>Faves Screen</Text>;
+  }
+}
+
+export default Faves;
---
+++ js/screens/Faves/FavesContainer.js
@@ -0,0 +1,8 @@
+import Faves from './Faves';
+import React from 'react';
+class FavesContainer extends React.Component {
+  render() {
+    return <Faves />;
+  }
+}
+export default FavesContainer;
