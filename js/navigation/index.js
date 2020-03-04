2fa14dafa584f27505e56a6713ea327ed9faba3d
Brooke <brooke.xiang@gmail.com>
2020-03-03 22:42:40 -0800

    add navigation to app

---
+++ js/navigation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BottomTabNav from './BottomTabNav';
+import {createStackNavigator} from '@react-navigation/stack';
+import SpeakerModal from '../screens/Speaker';
+const RootStack = createStackNavigator();
+const RootStackScreens = props => (
+  <RootStack.Navigator mode="modal" headerMode="none">
+    <RootStack.Screen name="BottomTab" component={BottomTabNav} />
+    <RootStack.Screen name="Speaker" component={SpeakerModal} />
+  </RootStack.Navigator>
+);
+
+export default RootStackScreens;
