eb355ae4e1ed3974c24bd8d0039eadc96f74e6c6
Brooke <brooke.xiang@gmail.com>
2020-03-04 20:46:31 -0800

    add AsyncStorage methods

---
+++ js/config/models.js
@@ -0,0 +1,30 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const addFave = faveId => {
+  try {
+    return AsyncStorage.setItem(
+      `${faveId}`,
+      JSON.stringify({id: faveId, faved_on: new Date()}),
+    );
+  } catch (error) {
+    return false;
+  }
+};
+
+export const removeFave = faveId => {
+  try {
+    return AsyncStorage.removeItem(`${faveId}`);
+  } catch (error) {
+    return true;
+  }
+};
+
+export const showFaves = async () => {
+  try {
+    const keys = await AsyncStorage.getAllKeys();
+    const allFaves = await AsyncStorage.multiGet(keys);
+    return allFaves.filter(session => session[1].includes('faved_on'));
+  } catch (error) {
+    throw error;
+  }
+};
