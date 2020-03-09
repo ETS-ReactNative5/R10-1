d05ab6e7f4455ff9f8612f320dafc08fbea036f4
Brooke <brooke.xiang@gmail.com>
2020-03-09 09:22:51 -0700

    add conduct component for about screen

---
+++ js/components/Conduct/Conduct.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {TouchableOpacity, Animated, Easing, Text} from 'react-native';
+import CustomText from '../../lib/customText/customText';
+import styles from './styles';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default class Conduct extends React.Component {
+  constructor(props) {
+    super(props);
+    this.spinValue = new Animated.Value(0);
+    this.opacityValue = new Animated.Value(0);
+    this.state = {
+      open: false,
+    };
+  }
+
+  animate = () => {
+    this.opacityValue.setValue(0);
+    this.spinValue.setValue(0);
+    const createAnimation = (value, duration, easing) => {
+      return Animated.timing(value, {
+        toValue: 1,
+        duration,
+        easing,
+      });
+    };
+    Animated.parallel([
+      createAnimation(this.opacityValue, 1000, Easing.ease),
+      createAnimation(this.spinValue, 500, Easing.linear),
+    ]).start();
+  };
+
+  render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg'],
+    });
+    const opacity = this.opacityValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, 1],
+    });
+    return (
+      <>
+        <TouchableOpacity
+          style={styles.conduct}
+          onPress={() => {
+            this.setState({open: !this.state.open});
+            this.animate();
+          }}>
+          <Animated.View
+            style={{
+              transform: [{rotate: spin}],
+            }}>
+            <MaterialCommunityIcons
+              style={styles.conductTitle}
+              name={this.state.open ? 'minus' : 'plus'}
+            />
+          </Animated.View>
+          <CustomText style={styles.conductTitle}>
+            {this.props.conductTitle}
+          </CustomText>
+        </TouchableOpacity>
+        {this.state.open ? (
+          <TouchableOpacity
+            onPress={() => {
+              this.animate();
+              this.setState({open: !this.state.open});
+            }}>
+            <Animated.View style={{...styles.conductDescription, opacity}}>
+              <CustomText> {this.props.conductDescription}</CustomText>
+            </Animated.View>
+          </TouchableOpacity>
+        ) : null}
+      </>
+    );
+  }
+}
---
+++ js/components/Conduct/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  conduct: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  conductTitle: {
+    color: '#9963ea',
+    fontSize: 16,
+    marginTop: 5,
+    marginBottom: 5,
+    fontWeight: '500',
+  },
+  conductDescription: {
+    fontSize: 16,
+  },
+});
+
+export default styles;
