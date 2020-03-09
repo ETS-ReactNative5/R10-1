5a4ff03e6332caebd176c7a3c879888b40771c2c
Brooke <brooke.xiang@gmail.com>
2020-03-09 09:24:32 -0700

    modify schedule screen after making scehdule list and schedule item

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -1,60 +1,13 @@
 import React from 'react';
-import moment from 'moment';
-import {
-  Text,
-  View,
-  SafeAreaView,
-  ScrollView,
-  TouchableOpacity,
-  SectionList,
-} from 'react-native';
-import {formatSessionData} from './../../lib/helper';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import styles from './styles';
-
-Item = ({session, navigation, faveIds}) => {
-  // console.log(faveIds);
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        console.log(session);
-        navigation.navigate('Session', {sessionInfo: session});
-      }}>
-      <View>
-        <Text>{session.title}</Text>
-        <View style={styles.faveLocation}>
-          <Text>{session.location}</Text>
-          {faveIds.faveIds.includes(session.id) ? (
-            <MaterialCommunityIcons name="heart" color="red" />
-          ) : null}
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-};
+import ScheduleList from '../../components/ScheduleList';
 
 const Schedule = ({navigation, sessions, faveIds}) => {
-  const sortedSchedule = formatSessionData(sessions);
-  console.log(sortedSchedule);
-
   return (
-    <ScrollView>
-      <View>
-        <SectionList
-          style={{}}
-          sections={sortedSchedule}
-          renderItem={({item}) => (
-            <Item session={item} navigation={navigation} faveIds={faveIds} />
-          )}
-          renderSectionHeader={({section: {title}}) => (
-            <Text style={styles.scheduleTitle}>
-              {moment(title).format('h:mm a')}
-            </Text>
-          )}
-        />
-      </View>
-    </ScrollView>
+    <ScheduleList
+      sessions={sessions}
+      navigation={navigation}
+      faveIds={faveIds}
+    />
   );
 };
 
--- js/screens/Schedule/ScheduleContainer.js
+++ js/screens/Schedule/ScheduleContainer.js
@@ -1,31 +1,16 @@
 import Schedule from './Schedule';
 import React, {Component} from 'react';
-import gql from 'graphql-tag';
+import {GET_ALL_SESSIONS} from '../../queries/queries';
 import {Query} from '@apollo/react-components';
-import {ActivityIndicator} from 'react-native';
+import Loader from '../../components/Loader';
 import {FavesContext} from '../../context/FavesContext';
-
-const GET_ALL_SESSIONS = gql`
-  {
-    allSessions {
-      id
-      description
-      location
-      speaker {
-        id
-      }
-      title
-      startTime
-    }
-  }
-`;
 
 class ScheduleContainer extends Component {
   render() {
     return (
       <Query query={GET_ALL_SESSIONS}>
         {({loading, error, data}) => {
-          if (loading) return <ActivityIndicator />;
+          if (loading) return <Loader />;
           if (error) return `Error! ${error.message}`;
           return (
             console.log(data),
@@ -35,7 +20,7 @@ class ScheduleContainer extends Component {
                   <Schedule
                     navigation={this.props.navigation}
                     sessions={data.allSessions}
-                    faveIds={faveIds}
+                    faveIds={faveIds.faveIds}
                   />
                 )}
               </FavesContext.Consumer>
